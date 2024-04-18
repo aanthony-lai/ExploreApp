f88fe5f93acadad782f5902c87729efe2f318449
Anthony Lai <Anthony.lai96@gmail.com>
2024-04-18 09:49:46 +0200

    Re-factor GooglePlacesService

--- ExploreApp/Builders/NearbySearchBuilder.cs
+++ ExploreApp/Builders/NearbySearchBuilder.cs
@@ -4,26 +4,38 @@ namespace ExploreApp.Builders
 {
 	public class NearbySearchBuilder: INearbySearchBuilder
 	{
-		private string[] types;
+		private string[] types = [];
 		private int maxResults;
 		private double latitude;
 		private double longitude;
 		private double radius;
+		private string rankPreference = string.Empty;
 
 		public NearbySearchBuilder()
 		{
-			types = [ "bar" ];
 			maxResults = 20;
 			latitude = 59.3318941;
 			longitude = 18.060944;
 			radius = 1000.0;
 		}
 
+		public NearbySearchBuilder setTypes(string type)
+		{
+			this.types = [ type ];
+			return this;
+		}
+
+		public NearbySearchBuilder setRankPreference(string rankPreference)
+		{
+			this.rankPreference = rankPreference;
+			return this;
+		}
+
 		public object Build()
 		{
 			return new
 			{
-				includedTypes = this.types,
+				includedPrimaryTypes = this.types,
 				maxResultCount = this.maxResults,
 				locationRestriction = new
 				{
--- ExploreApp/Services/GooglePlacesService.cs
+++ ExploreApp/Services/GooglePlacesService.cs
@@ -1,11 +1,13 @@
 ﻿using ExploreApp.DTO.NearbySearchDTOs;
 using ExploreApp.DTO.TextSearchDTOs;
 using ExploreApp.Interfaces;
+using ExploreApp.NewFolder;
 using System.Text.Json;
+
 
 namespace ExploreApp.Services
 {
-	public class GooglePlacesService: IGooglePlacesService
+	public class GooglePlacesService : IGooglePlacesService
 	{
 		private readonly IHttpClientFactory _httpClient;
 		private readonly IConfiguration _configuration;
@@ -24,34 +26,45 @@ namespace ExploreApp.Services
 			_nearbySearchBuilder = nearbySearchBuilder;
 		}
 
-		public async Task<NearbySearchDTO> NearbySearchAsync()
+		public async Task<NearbySearchDTO> NearbySearchAsync(string type)
 		{
 			const string serviceType = "NearbySearch";
 			var client = _httpClient.CreateClient("GoogleClient");
 			_googleHttpClientConfigurer.ConfigureHeaders(client, serviceType);
-			
-			var content = _nearbySearchBuilder.Build();
 
+			var content = _nearbySearchBuilder
+				.setTypes(type)
+				.Build();
+			
 			var response = await client.PostAsJsonAsync("https://places.googleapis.com/v1/places:searchNearby", content);
 			string responseBody = await response.Content.ReadAsStringAsync();
 			return JsonSerializer.Deserialize<NearbySearchDTO>(responseBody)
 				?? new NearbySearchDTO();
 		}
 
-		public async Task<TextSearchDTO> TextSearchAsync(string textQuery, string type, string price)
+		public async Task<TextSearchDTO> TextSearchAsync(SearchModel searchModel)
 		{
 			const string serviceType = "TextSearch";
 			var client = _httpClient.CreateClient("GoogleClient");
 			_googleHttpClientConfigurer.ConfigureHeaders(client, serviceType);
-			
+
 			var content = _textSearchBuilder
-				.setQuery(textQuery)
+				.setQuery(searchModel.textQuery)
+				.setPrice(searchModel.selectedPrice)
+				.setType(searchModel.selectedType)
 				.Build();
 
 			var response = await client.PostAsJsonAsync("https://places.googleapis.com/v1/places:searchText", content);
 			string responseBody = await response.Content.ReadAsStringAsync();
 			return JsonSerializer.Deserialize<TextSearchDTO>(responseBody)
 				?? new TextSearchDTO();
+		}
+
+		public string GetPlacePhoto(string placeName)
+		{
+			var apiKey = _configuration.GetValue<string>("GoogleAPI:APIKey");
+			string imageUrl = $"https://places.googleapis.com/v1/{placeName}/media?key={apiKey}&maxWidthPx=1000";
+			return imageUrl;
 		}
 	}
 }
